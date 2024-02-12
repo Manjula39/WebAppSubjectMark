4a1294c60c534b377e92af383869f260f56018c0
Manjula36 <manjulavdt@gmail.com>
2024-02-12 13:51:04 +0530

    Add project files.

---
+++ WebAppSubjectMark/Data/SubjectsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppSubjectMark.Models;
+
+namespace WebAppSubjectMark.Data
+{
+    public class SubjectsDbContext : DbContext
+    {
+        public SubjectsDbContext (DbContextOptions<SubjectsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppSubjectMark.Models.Subjects> Subjects { get; set; } = default!;
+    }
+}
---
+++ WebAppSubjectMark/Models/Subjects.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppSubjectMark.Models
+{
+    public class Subjects
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string? StudentName { get; set; }
+        public string? SubjectName { get; set; }
+        public int SubjectMarks { get; set; }
+    }
+}
